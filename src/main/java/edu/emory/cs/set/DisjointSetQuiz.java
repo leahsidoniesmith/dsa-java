c74a12870e11974338ab76783b7b1c1c24ea7544
Leah Smith <leah.sidonie.smith@emory.edu>
2020-11-06 18:12:46 -0500

    Quiz 6

--- src/main/java/edu/emory/cs/set/DisjointSetQuiz.java
+++ src/main/java/edu/emory/cs/set/DisjointSetQuiz.java
@@ -4,14 +4,9 @@ package edu.emory.cs.set;
 public class DisjointSetQuiz {
     static public void main(String[] args) {
         DisjointSet ds = new DisjointSet(5);
-        System.out.println(ds.toString());
         ds.union(2, 3);
-        System.out.println(ds.toString());
         ds.union(4, 3);
-        System.out.println(ds.toString());
         ds.union(0, 1);
-        System.out.println(ds.toString());
         ds.union(1, 3);
-        System.out.println(ds.toString());
     }
 }
